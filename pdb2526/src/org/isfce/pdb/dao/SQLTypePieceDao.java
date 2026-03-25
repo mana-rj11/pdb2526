15198e674ad93bbd34d7c7b83c3ca4a4dfd8eb40
VOZ <voisfce@gmail.com>
2026-03-25 21:42:24 +0100

    daoTypePiece+test

--- pdb2526/src/org/isfce/pdb/dao/SQLTypePieceDao.java
+++ pdb2526/src/org/isfce/pdb/dao/SQLTypePieceDao.java
@@ -1,30 +1,65 @@
 package org.isfce.pdb.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.isfce.pdb.model.TypePiece;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class SQLTypePieceDao implements ITypePieceDao {
+	private static String SQL_GET_FROM_ID="""
+			select t.CODE_TYP,t.NOM_TYP,t.HUMIDE_TYP from TTYPE_PIECE t
+			where t.CODE_TYP= ?
+			""";
+	private static String SQL_GET_LISTE="""
+			select t.CODE_TYP,t.NOM_TYP,t.HUMIDE_TYP from TTYPE_PIECE t
+			order by t.CODE_TYP
+			""";
 	//private DAOFactory factory; //pas besoin pour les TypePiece
 	private Connection connexion;
 
 	public SQLTypePieceDao(DAOFactory factory) {
-		//this.factory = factory;
 		this.connexion = factory.getConnection();
 	}
 
 	@Override
 	public Optional<TypePiece> getFromID(String id) {
-		// TODO Auto-generated method stub
-		return ITypePieceDao.super.getFromID(id);
+		TypePiece obj=null;
+		try(PreparedStatement ps=connexion.prepareStatement(SQL_GET_FROM_ID)){
+			ps.setString(1,id.trim().toUpperCase());
+			ResultSet rs=ps.executeQuery();
+			if(rs.next()) {
+				obj=new TypePiece(rs.getString("CODE_TYP").trim(), rs.getString("NOM_TYP"),
+						rs.getBoolean("HUMIDE_TYP"));
+				log.debug("Un type de pièce est créé: "+obj);
+			}
+		} catch (SQLException e) {
+			log.error(e.getMessage());
+		};
+		return Optional.ofNullable(obj);
 	}
 
 	@Override
 	public List<TypePiece> getListe(String regExpr) {
-		// TODO Auto-generated method stub
-		return ITypePieceDao.super.getListe(regExpr);
+		 List<TypePiece> liste=new ArrayList<>(20);
+		 try(Statement st=connexion.createStatement()){
+				ResultSet rs=st.executeQuery(SQL_GET_LISTE);
+				while(rs.next()) {
+					liste.add(new TypePiece(rs.getString("CODE_TYP").trim(), rs.getString("NOM_TYP"),
+							rs.getBoolean("HUMIDE_TYP")));
+				}
+			} catch (SQLException e) {
+				log.error(e.getMessage());
+			};
+		return liste;
 	}
 
 }
--- pdb2526/tests/org/isfce/pdb/dao/TestDaoTypePiece.java
+++ pdb2526/tests/org/isfce/pdb/dao/TestDaoTypePiece.java
@@ -3,29 +3,31 @@ package org.isfce.pdb.dao;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 
+import org.isfce.pdb.dao.DAOFactory.TypePersistance;
 import org.isfce.pdb.databases.connexion.ConnexionFromFile;
 import org.isfce.pdb.databases.connexion.ConnexionSingleton;
 import org.isfce.pdb.databases.uri.Databases;
+import org.isfce.pdb.model.TypePiece;
 import org.isfce.pdb.util.DatabaseUtil;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+public class TestDaoTypePiece {
 
-class TestDaoTypePiece {
-	
- private static Connection connect;
-	
+	private static ITypePieceDao dao;
+	static TypePiece p = new TypePiece("SALON", "Salon", false);
+
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 		ConnexionSingleton.setInfoConnexion(
 				new ConnexionFromFile("./ressources/connexionPDB2526_test.properties", Databases.FIREBIRD));
 		// Réinitialise la base de données dans son état initial
 		DatabaseUtil.executeScriptSQL(ConnexionSingleton.getConnexion(), "./ressources/scriptInitDBTest.sql");
-		connect=ConnexionSingleton.getConnexion();
+		var factory = DAOFactory.getDAOFactory(TypePersistance.FIREBIRD, ConnexionSingleton.getConnexion());
+		dao = factory.getTypePieceDAO();
 	}
 
 	@AfterAll
@@ -37,13 +39,16 @@ class TestDaoTypePiece {
 
 	@Test
 	void testGetFromId() throws SQLException {
-		var st= connect.createStatement();
-		boolean res=st.execute("SELECT COUNT(*) FROM TTYPE_PIECE");
-		assertTrue(res);
-		var rs=st.getResultSet();
-		assertTrue(rs.next());
-		assertEquals(3, rs.getInt(1));
+		var oObj = dao.getFromID("salon");
+		assertTrue(oObj.isPresent());
+		assertEquals(p, oObj.get());
 	}
 
+	@Test
+	void testGetListe() throws SQLException {
+		var liste = dao.getListe(null);
+		assertTrue(liste.size() == 3);
+		assertEquals(p, liste.get(1));
+	}
 
 }
