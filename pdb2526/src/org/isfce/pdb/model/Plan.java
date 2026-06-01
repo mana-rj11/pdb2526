2ede365eea3d8ea3507684e80941ae7c263fcd85
Nilton Mana <Niltonmana11@gmail.com>
2026-06-01 13:59:58 +0200

    ajouter Plan model class

---
+++ pdb2526/src/org/isfce/pdb/model/Plan.java
@@ -0,0 +1,16 @@
+package org.isfce.pdb.model;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+public class Plan {
+	private final int id;
+	private final String fichier;
+
+}
