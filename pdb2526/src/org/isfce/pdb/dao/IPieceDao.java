1e92bbd0822d960a7d42a9d40e90064662ec0142
Nilton Mana <Niltonmana11@gmail.com>
2026-06-09 21:50:07 +0200

    maj + ajout getListeFromInstallation

--- pdb2526/src/org/isfce/pdb/dao/IPieceDao.java
+++ pdb2526/src/org/isfce/pdb/dao/IPieceDao.java
@@ -1,7 +1,11 @@
 package org.isfce.pdb.dao;
 
+import java.util.List;
+
 import org.isfce.pdb.model.Piece;
 
 public interface IPieceDao extends IDAO<Piece, Integer> {
+	
+	List<Piece> getListeFromInstallation(Integer installation);
 
 }
