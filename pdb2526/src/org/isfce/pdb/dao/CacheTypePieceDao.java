f63b119152b33d6d62e943c13baa5c331e0cb51f
Nilton Mana <Niltonmana11@gmail.com>
2026-06-16 20:23:50 +0200

    ajout clone

--- pdb2526/src/org/isfce/pdb/dao/CacheTypePieceDao.java
+++ pdb2526/src/org/isfce/pdb/dao/CacheTypePieceDao.java
@@ -4,12 +4,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.isfce.pdb.model.TypePiece;
 
 public class CacheTypePieceDao implements ITypePieceDao {
 	private ITypePieceDao dao;
 	private Map<String, TypePiece> cache = new HashMap<String, TypePiece>();
+	private boolean firstLoad = true;
 
 	public CacheTypePieceDao(ITypePieceDao dao) {
 		this.dao = dao;
@@ -18,28 +20,33 @@ public class CacheTypePieceDao implements ITypePieceDao {
 	@Override
 	public Optional<TypePiece> getFromID(String id) {
 		if (cache.containsKey(id))// existe dans le cache?
-			return Optional.of(cache.get(id));
+			return Optional.of(cache.get(id).clone());
 		// va chercher l'objet en BD
 		var oObj = dao.getFromID(id);
 		// rajoute l'objet au cache s'il existe
 		oObj.ifPresent((obj) -> cache.put(id, obj));
-		return oObj;
+		return oObj.map(TypePiece::clone);
 	}
 
 	@Override
 	public List<TypePiece> getListe(String regExpr) {
-		var liste = dao.getListe(null);
-		//cache.clear();
-		//rajoute tous les objets dans le cache
-		liste.forEach((e) -> cache.put(e.getCode(), e));
-		return liste;
+		if (firstLoad) {
+			var liste = dao.getListe(null);
+			//cache.clear();
+			//rajoute tous les objets dans le cache
+			liste.forEach((e) -> cache.put(e.getCode(), e));
+			firstLoad = false;
+		}
+		// renvoie des clones depuis le cache, jamais les originaux
+		return cache.values().stream().map(TypePiece::clone).collect(Collectors.toList());
+		
 	}
 
 	@Override
 	public TypePiece insert(TypePiece objet) throws Exception {
 		objet = dao.insert(objet);
 		cache.put(objet.getCode(), objet);
-		return objet;
+		return objet.clone();
 	}
 
 }
