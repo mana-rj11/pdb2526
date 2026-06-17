d5bc32db6ede1e82b71e5c182182db1a28b7c2f8
Nilton Mana <Niltonmana11@gmail.com>
2026-06-17 12:06:30 +0200

    ajout PLACE_LOC dans le model

--- pdb2526/src/org/isfce/pdb/model/Localisation.java
+++ pdb2526/src/org/isfce/pdb/model/Localisation.java
@@ -11,6 +11,8 @@ import lombok.ToString;
  * Représente la position d'un élément sur un plan 
  * x, y : position sur le plan
  * a : angle de rotation (0 par défaut = horizontal)
+ * place : indique si l'élément est réellement positionné sur le plan (true)
+ *		   ou juste assigné à une pièce sans position précise encore (false) 
  */
 @Getter
 @Setter
@@ -21,4 +23,5 @@ public class Localisation {
 	private double x;
 	private double y;
 	private double a;
+	private boolean place;
 }
