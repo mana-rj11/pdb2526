dfecb087e340b9820131084f024ab402260a5e15
Nilton Mana <Niltonmana11@gmail.com>
2026-06-17 22:45:05 +0200

    Methode pour charge installation

--- pdb2526/src/org/isfce/pdb/services/Facade.java
+++ pdb2526/src/org/isfce/pdb/services/Facade.java
@@ -1,14 +1,13 @@
 package org.isfce.pdb.services;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 import org.isfce.pdb.dao.DAOFactory;
 import org.isfce.pdb.exceptions.InstallationException;
-import org.isfce.pdb.model.Adresse;
 import org.isfce.pdb.model.Installation;
 import org.isfce.pdb.model.Piece;
+import org.isfce.pdb.model.Plan;
 import org.isfce.pdb.model.TypePiece;
 import org.isfce.pdb.view.bundle.I18N;
 
@@ -36,22 +35,8 @@ public class Facade {
 	 */
 	
 	public void chargeInstallation(int id) throws InstallationException {
-		if (id == 1) {
-			installation = Installation.builder()
-					.adresse(new Adresse("Rue J buedts", 1040, "Etterbeek"))
-					.date(LocalDate.of(2026, 5, 12))
-					.id(1)
-					.Installateur("moi")
-					.proprietaire("truc").build();
-				// .getFromId(id)
-			//.orElseThrow(() -> new InstallationException("Installation introuvable : " + id));
-			//pieces = factory.getPieceDAO().getListe(null);
-			//elements = factory.getElementDAO().getListeFromInstallation(id);
-			//logger.info("Installation chargée : " + id
-			//	+ " | pièces : " + pieces.size()
-			//	+ " | éléments : " + elements.size());
-		} else 
-			throw new InstallationException(I18N.getString("err.install.inconnue"));
+		installation = factory.getInstallationDAO().getFromId(id)
+				.orElseThrow(() -> new InstallationException(I18N.getString("err.install.inconnue")));
 	}
 	
 	/**
@@ -66,9 +51,22 @@ public class Facade {
 		return installation;
 	}
 	
+	
+	// -----------------------------------------------------------
+	// Plan
+	// -----------------------------------------------------------
+	
+	/**
+	 * Ajoute un nouveau plan \u00e0 l'installation courante
+	 */
+	public Plan insertPlan(Plan plan) throws InstallationException {
+		return factory.getPlanDAO().insert(plan, getCurrentInstallation().getId());
+	}
+	
 	// -----------------------------------------------------------
 	// TypePiece
 	// -----------------------------------------------------------
+	
 	
 	/**
 	 * Retourne tous les types de pièces
